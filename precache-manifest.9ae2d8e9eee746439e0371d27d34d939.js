9e4781a8c59f38f0733fba135dd563e568e2563c
stella <masarupa@gmail.com>
2019-06-18 00:04:46 -0400

    Updates

---
+++ precache-manifest.9ae2d8e9eee746439e0371d27d34d939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e7760a7e1bf02420a270ae0bf73e90",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "727d6db80628142d0a4d",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/my-app/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "727d6db80628142d0a4d",
+    "url": "/my-app/static/js/main.036aed09.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
